be4eb1555f1b6178797cf9a75f431c1e6d3913c7
JamesMcGuigan <james.mcguigan.github@gmail.com>
2017-04-17 11:42:41 +0100

    Add support for Alexa Cards + HelpIntent + StopIntent + CancelIntent

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 require('datejs');
+require('datejs/src/i18n/en-GB.js');
 var alexa = require('alexa-app');
 var discordianDate = require('./discordianDate');
 
@@ -7,10 +8,11 @@ module.change_code = 1;
 
 // Define an alexa-app
 var app = new alexa.app('alexa-discordian-date');
-
+app.dateFormat = 'dddd ddS MMMM yyyy'; // 'Monday 17th April 2017'
 
 app.launch(function(request, response) {
-	response.say("Today is... " + discordianDate());
+	response.say("Ask Discordian Date for today, yesterday or even May 3rd 2034, which date would you like the discordian for?");
+	response.shouldEndSession(false, "Pick a date, any date. Discordian Date will do the rest.");
 });
 app.intent('DiscordianDateTodayIntent',
 	{
@@ -19,7 +21,14 @@ app.intent('DiscordianDateTodayIntent',
 		]
 	},
 	function(request, response) {
-		response.say("Today is... " + discordianDate());
+		var date = Date.today();
+		var gregorian = Date.parse(date).toString(app.dateFormat);
+		response.say(gregorian + "is..." + discordianDate(date));
+		response.card({
+			"type":    "Simple",
+			"title":   discordianDate(),
+			"content": gregorian
+		})
 	}
 );
 app.intent('DiscordianDateIntent',
@@ -28,22 +37,34 @@ app.intent('DiscordianDateIntent',
       "Date":     "AMAZON.DATE"
 		},
 		"utterances":[
-			"{for|} {the|} {discordian|} {date|} {Date}"
+			"{for|} {the|} {discordian|} {date|} {-|Date}"
 		]
 	},
   function(request, response) {
 		var date = Date.parse(request.slot('Date'));
-
 		if( !date ) {
 			response.say("Fnord!");
 			return;
 		}
-		if( date.same().day(Date.today()) ) {
-			response.say("Today is... ");
-		} else {
-			response.say(request.slot('Date') + " is... ");
-		}
-		response.say(discordianDate(date));
+		var gregorian = Date.parse(date).toString(app.dateFormat);
+		response.say(gregorian + "is..." + discordianDate(date));
+		response.card({
+			"type":    "Simple",
+			"title":   discordianDate(),
+			"content": gregorian
+		})
 	}
 );
+app.intent('AMAZON.HelpIntent', {}, function(request, response) {
+	response.say("Ask Discordian Date for today, yesterday or even May 3rd 2034, which date would you like the discordian for?");
+	response.shouldEndSession(false, "Pick a date, any date. Discordian Date will do the rest.");
+});
+app.intent('AMAZON.StopIntent', {}, function(request, response) {
+	response.say("Fnord!");
+});
+app.intent('AMAZON.CancelIntent', {}, function(request, response) {
+	response.say("Fnord!");
+});
+
+
 module.exports = app;
